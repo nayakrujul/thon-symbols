3e75849938810d985c4c9cee9142278600359c53
Rujul Nayak <55329600+nayakrujul@users.noreply.github.com>
2022-10-22 07:18:34 +0100

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -4,7 +4,7 @@ long_description = 'A very concise language - read the docs at https://www.githu
 
 setup(
   name = 'thon-symbols',
-  version = '1.12',
+  version = '1.13',
   license='Apache',
   description = 'A very concise language',
   author = 'Rujul Nayak',
